05f5bf170d47b2f635b75082d870252bc5833bfc
Luis Fernando Palomeque Ortiz <fernando18092105@gmail.com>
2020-02-08 14:54:34 -0600

    NEW SERVER

--- SilverOnline/php/Conexion.php
+++ SilverOnline/php/Conexion.php
@@ -3,7 +3,7 @@
 function conexion(){
 
   // $serverName = "DESKTOP-9Q38T8N\SQLEXPRESS";
-  $serverName = "192.168.3.114";
+  $serverName = "192.168.3.100";
   $connectionInfo = array( "Database"=>"SAE61", "UID"=>"sa", "PWD"=>"Silver2020");
   $conn = sqlsrv_connect( $serverName, $connectionInfo);
   // if( $conn ) {
