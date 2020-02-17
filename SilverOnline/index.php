0dc37e8202b2c884389aa4276983a11458148c82
LPALOMEQUE <fer18092105@icloud.com>
2020-02-17 15:06:56 -0600

    Presentacion de pagina inicial.

--- SilverOnline/index.php
+++ SilverOnline/index.php
@@ -73,7 +73,7 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title  -->
-    <title>Silver - Evolution | Home</title>
+    <title>Silver - Evolution | Inicio</title>
 
     <!-- Favicon  -->
     <link rel="icon" href="img/core-img/favicon.ico">
@@ -260,10 +260,10 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
       <div class="col-12 d-md-flex justify-content-between">
         <!-- Header Social Area -->
         <div class="header-social-area">
-          <a href="#"><span class="karl-level">Share</span> <i class="fa fa-pinterest" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-          <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+          <!-- <a href="#"><span class="karl-level">Comparte</span> <i class="fa fa-pinterest" aria-hidden="true"></i></a> -->
+          <a href="https://es-la.facebook.com/newsilverevolution/"><span class="karl-level">Comparte</span><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <!-- <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a> -->
+          <!-- <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a> -->
         </div>
         <!-- Modal para inicio de sesion -->
         <div class="modal fade" id="ModalLogin" tabindex="-1" role="dialog" aria-labelledby="ModalLogin" aria-hidden="true">
@@ -306,19 +306,23 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
 
             <div class="collapse navbar-collapse align-items-start collapse" id="karl-navbar">
               <ul class="navbar-nav animated" id="nav">
+                <li class="nav-item active"><a class="nav-link" href="#"></a></li>
+                <li class="nav-item active"><a class="nav-link" href="#"></a></li>
+                <li class="nav-item active"><a class="nav-link" href="#"></a></li>
+                <li class="nav-item active"><a class="nav-link" href="#"></a></li>
+
                 <li class="nav-item active"><a class="nav-link" href="index.php">Inicio</a></li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="karlDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Paginas</a>
+                  <a class="nav-link dropdown-toggle" href="#" id="karlDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Categorías</a>
                   <div class="dropdown-menu" aria-labelledby="karlDropdown">
-                    <a class="dropdown-item" href="index.php">Inicio</a>
-                    <a class="dropdown-item" href="shop.html">Compras</a>
-                    <a class="dropdown-item" href="product-details.html">Detalles de productos</a>
-                    <a class="dropdown-item" href="cart.html">Carrito</a>
-                    <a class="dropdown-item" href="checkout.html">Resiva</a>
+                    <a class="dropdown-item" href="joyas-m.php">Joyería</a>
+                    <a class="dropdown-item" href="#">Bolsas</a>
+                    <a class="dropdown-item" href="#">Perfumes</a>
+                    <a class="dropdown-item" href="#">Ropa</a>
                   </div>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#ModalRegistroUsuarios">Sign In</a></li>
-                <div class="<?php
+                <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#ModalRegistroUsuarios">Registrate</a></li>
+                <!-- <div class="<?php
                 if (isset($_SESSION["status"]) && $_SESSION["status"] == 'ADMIN') {
                   echo $category = 'inline';
                 }else {
@@ -326,7 +330,7 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
                 } ?>">
                 <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#ModalArticulos">Add Articulos</a></li>
 
-              </div>
+              </div> -->
             </ul>
           </div>
         </nav>
@@ -569,18 +573,18 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
 <section class="top-discount-area d-md-flex align-items-center">
   <!-- Single Discount Area -->
   <div class="single-discount-area">
-    <h5>Envio Gratis &amp; Apresurate</h5>
-    <h6><a href="#">COMPRA YA</a></h6>
+    <h5>Apresurate &amp; Atrevete a ganar mas</h5>
+    <h6><a href="#">Compra ya</a></h6>
   </div>
   <!-- Single Discount Area -->
   <div class="single-discount-area">
-    <h5>20% Discount for all dresses</h5>
-    <h6>USE CODE: Colorlib</h6>
+    <h5>Silver Evolution</h5>
+    <h6>Tu mejor opción</h6>
   </div>
   <!-- Single Discount Area -->
   <div class="single-discount-area">
-    <h5>20% Discount for students</h5>
-    <h6>USE CODE: Colorlib</h6>
+    <h5>Empresa 100% Mexicana</h5>
+    <h6>Crecé con nosotros</h6>
   </div>
 </section>
 <!-- ****** Top Discount Area End ****** -->
@@ -594,9 +598,9 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
         <div class="row h-100 align-items-center">
           <div class="col-12">
             <div class="welcome_slide_text">
-              <h6 data-animation="bounceInDown" data-delay="0" data-duration="500ms">* Only today we offer free shipping</h6>
-              <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Fashion Trends</h2>
-              <a href="#" class="btn karl-btn" data-animation="fadeInUp" data-delay="1s" data-duration="500ms">Shop Now</a>
+              <h6 data-animation="bounceInDown" data-delay="0" data-duration="500ms">* Excelencia y calidad</h6>
+              <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Tendencia de moda</h2>
+              <a href="joyas-m.php" class="btn karl-btn" data-animation="fadeInUp" data-delay="1s" data-duration="500ms">Compra Ya</a>
             </div>
           </div>
         </div>
@@ -609,7 +613,7 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
         <div class="row h-100 align-items-center">
           <div class="col-12">
             <div class="welcome_slide_text">
-              <h6 data-animation="fadeInDown" data-delay="0" data-duration="500ms">* Only today we offer free shipping</h6>
+              <h6 data-animation="fadeInDown" data-delay="0" data-duration="500ms">* Excelencia y calidad</h6>
               <h2 data-animation="fadeInUp" data-delay="500ms" data-duration="500ms">Summer Collection</h2>
               <a href="#" class="btn karl-btn" data-animation="fadeInLeftBig" data-delay="1s" data-duration="500ms">Check Collection</a>
             </div>
@@ -619,7 +623,7 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
     </div>
 
     <!-- Single Slide Start -->
-    <div class="single_slide height-800 bg-img background-overlay" style="background-image: url(img/bg-img/bg-2.jpg);">
+    <!-- <div class="single_slide height-800 bg-img background-overlay" style="background-image: url(img/bg-img/bg-2.jpg);">
       <div class="container h-100">
         <div class="row h-100 align-items-center">
           <div class="col-12">
@@ -631,7 +635,7 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
           </div>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </section>
 <!-- ****** Welcome Slides Area End ****** -->
@@ -641,17 +645,17 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
   <!-- Single Catagory -->
   <div class="single_catagory_area d-flex align-items-center bg-img" style="background-image: url(img/bg-img/bg-2.jpg);">
     <div class="catagory-content">
-      <h6>On Accesories</h6>
-      <h2>Sale 30%</h2>
-      <a href="#" class="btn karl-btn">SHOP NOW</a>
+      <!-- <h6>On Accesories</h6> -->
+      <h2>precio especial</h2>
+      <!-- <a href="#" class="btn karl-btn">SHOP NOW</a> -->
     </div>
   </div>
   <!-- Single Catagory -->
   <div class="single_catagory_area d-flex align-items-center bg-img" style="background-image: url(img/bg-img/bg-3.jpg);">
     <div class="catagory-content">
-      <h6>in Bags excepting the new collection</h6>
-      <h2>Designer bags</h2>
-      <a href="#" class="btn karl-btn">SHOP NOW</a>
+      <!-- <h6>in Bags excepting the new collection</h6> -->
+      <h2>Nuestros diseños</h2>
+      <!-- <a href="#" class="btn karl-btn">SHOP NOW</a> -->
     </div>
   </div>
 </section>
@@ -709,7 +713,7 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
                 </form>
 
                 <div class="share_wf mt-30">
-                  <p>Share With Friend</p>
+                  <p>Comparte con tus amigos</p>
                   <div class="_icon">
                     <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
                     <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
@@ -727,162 +731,6 @@ if(isset($_POST['ID']) && isset($_POST['PRECIO']) && isset($_POST['CANTIDAD'])) 
 </div>
 <!-- ****** Quick View Modal Area End ****** -->
 
-<!-- ****** New Arrivals Area Start ****** -->
-<section class="new_arrivals_area section_padding_100_0 clearfix">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="section_heading text-center">
-          <h2>New Arrivals</h2>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="karl-projects-menu mb-100">
-    <div class="text-center portfolio-menu">
-      <button class="btn active" data-filter="*">ALL</button>
-      <button class="btn" data-filter=".women">WOMAN</button>
-      <button class="btn" data-filter=".man">MAN</button>
-      <button class="btn" data-filter=".access">ACCESSORIES</button>
-      <button class="btn" data-filter=".shoes">shoes</button>
-      <button class="btn" data-filter=".kids">KIDS</button>
-    </div>
-  </div>
-
-  <div class="container">
-    <div class="row karl-new-arrivals">
-
-      <!-- Single gallery Item Start -->
-      <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.2s">
-        <!-- Product Image -->
-        <div class="product-img">
-          <img src="img/product-img/product-1.jpg" alt="">
-          <div class="product-quicview">
-            <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-          </div>
-        </div>
-        <!-- Product Description -->
-        <div class="product-description">
-          <h4 class="product-price">$39.90</h4>
-          <p>Jeans midi cocktail dress</p>
-          <!-- Add to Cart -->
-          <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-        </div>
-      </div>
-
-      <!-- Single gallery Item Start -->
-      <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.3s">
-        <!-- Product Image -->
-        <div class="product-img">
-          <img src="img/product-img/product-2.jpg" alt="">
-          <div class="product-quicview">
-            <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-          </div>
-        </div>
-        <!-- Product Description -->
-        <div class="product-description">
-          <h4 class="product-price">$39.90</h4>
-          <p>Jeans midi cocktail dress</p>
-          <!-- Add to Cart -->
-          <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-        </div>
-      </div>
-
-      <!-- Single gallery Item Start -->
-      <div class="col-12 col-sm-6 col-md-4 single_gallery_item access wow fadeInUpBig" data-wow-delay="0.4s">
-        <!-- Product Image -->
-        <div class="product-img">
-          <img src="img/product-img/product-3.jpg" alt="">
-          <div class="product-quicview">
-            <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-          </div>
-        </div>
-        <!-- Product Description -->
-        <div class="product-description">
-          <h4 class="product-price">$39.90</h4>
-          <p>Jeans midi cocktail dress</p>
-          <!-- Add to Cart -->
-          <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-        </div>
-      </div>
-
-      <!-- Single gallery Item Start -->
-      <div class="col-12 col-sm-6 col-md-4 single_gallery_item shoes wow fadeInUpBig" data-wow-delay="0.5s">
-        <!-- Product Image -->
-        <div class="product-img">
-          <img src="img/product-img/product-4.jpg" alt="">
-          <div class="product-quicview">
-            <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-          </div>
-        </div>
-        <!-- Product Description -->
-        <div class="product-description">
-          <h4 class="product-price">$39.90</h4>
-          <p>Jeans midi cocktail dress</p>
-          <!-- Add to Cart -->
-          <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-        </div>
-      </div>
-
-      <!-- Single gallery Item Start -->
-      <div class="col-12 col-sm-6 col-md-4 single_gallery_item women wow fadeInUpBig" data-wow-delay="0.6s">
-        <!-- Product Image -->
-        <div class="product-img">
-          <img src="img/product-img/product-5.jpg" alt="">
-          <div class="product-quicview">
-            <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-          </div>
-        </div>
-        <!-- Product Description -->
-        <div class="product-description">
-          <h4 class="product-price">$39.90</h4>
-          <p>Jeans midi cocktail dress</p>
-          <!-- Add to Cart -->
-          <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-        </div>
-      </div>
-
-      <!-- Single gallery Item -->
-      <div class="col-12 col-sm-6 col-md-4 single_gallery_item kids man wow fadeInUpBig" data-wow-delay="0.7s">
-        <!-- Product Image -->
-        <div class="product-img">
-          <img src="img/product-img/product-6.jpg" alt="">
-          <div class="product-quicview">
-            <a href="#" data-toggle="modal" data-target="#quickview"><i class="ti-plus"></i></a>
-          </div>
-        </div>
-        <!-- Product Description -->
-        <div class="product-description">
-          <h4 class="product-price">$39.90</h4>
-          <p>Jeans midi cocktail dress</p>
-          <!-- Add to Cart -->
-          <a href="#" class="add-to-cart-btn">ADD TO CART</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- ****** New Arrivals Area End ****** -->
-
-<!-- ****** Offer Area Start ****** -->
-<section class="offer_area height-700 section_padding_100 bg-img" style="background-image: url(img/bg-img/bg-5.jpg);">
-  <div class="container h-100">
-    <div class="row h-100 align-items-end justify-content-end">
-      <div class="col-12 col-md-8 col-lg-6">
-        <div class="offer-content-area wow fadeInUp" data-wow-delay="1s">
-          <h2>White t-shirt <span class="karl-level">Hot</span></h2>
-          <p>* Free shipping until 25 Dec 2017</p>
-          <div class="offer-product-price">
-            <h3><span class="regular-price">$25.90</span> $15.90</h3>
-          </div>
-          <a href="#" class="btn karl-btn mt-30">Shop Now</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- ****** Offer Area End ****** -->
 
 <!-- ****** Popular Brands Area Start ****** -->
 <section class="karl-testimonials-area section_padding_100">
